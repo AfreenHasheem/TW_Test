b57eff5b4deebac4eeb0af531f585071cda5e92b
Afreen Siraj <afreen.siraj@capco.com>
2019-10-09 22:48:56 +0100

    trying offline caching

--- app/src/main/java/com/example/tw_test/hero_list/HeroListModel.java
+++ app/src/main/java/com/example/tw_test/hero_list/HeroListModel.java
@@ -6,7 +6,14 @@ import com.example.tw_test.adapter.HeroAdapter;
 import com.example.tw_test.model.Hero;
 import com.example.tw_test.network.ApiClient;
 import com.example.tw_test.network.ApiInterface;
+
+import java.io.IOException;
 import java.util.List;
+
+import okhttp3.Cache;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -18,6 +25,26 @@ public class HeroListModel implements HeroListContract.Model {
 
     @Override
     public void getHeroList(final OnFinishedListener onFinishedListener) {
+//
+//        Cache cache = new Cache(getCacheDir(), cacheSize);
+//        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .cache(cache)
+//                .addInterceptor(new Interceptor() {
+//                    @Override
+//                    public okhttp3.Response intercept(Interceptor.Chain chain) throws IOException {
+//                        Request request = chain.request();
+//                        if (!mConnectionDetector.isConnectingToInternet()){
+//                            int maxStale = 60 * 60 * 24 * 28; // tolerate 4-weeks stale \
+//                            request = request
+//                                    .newBuilder()
+//                                    .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+//                                    .build();
+//                        }
+//
+//                        return chain.proceed(request);
+//                    }
+//                })
+//                .build();
 
         ApiInterface apiService =
                 ApiClient.getClient().create(ApiInterface.class);
--- app/src/main/java/com/example/tw_test/network/ConnectionDetector.java
+++ app/src/main/java/com/example/tw_test/network/ConnectionDetector.java
@@ -6,112 +6,17 @@ import android.net.ConnectivityManager;
 public class ConnectionDetector {
     private Context mContext;
 
-    public ConnectionDetector(Context context){
+    public ConnectionDetector(Context context) {
         this.mContext = context;
     }
 
-    public boolean isConnectingToInternet(){
+    public boolean isConnectingToInternet() {
 
-        ConnectivityManager cm = (ConnectivityManager)mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if(cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected() == true)
-        {
+        ConnectivityManager cm = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected() == true) {
             return true;
         }
         return false;
     }
-
-//    private void loadHeroes(){
-//
-//        try{
-//            if (Api.BASE_URL.isEmpty()){
-//                Toast.makeText(getApplicationContext(), "Please obtain API Key firstly from themoviedb.org", Toast.LENGTH_SHORT).show();
-//                progressDialog.dismiss();
-//                return;
-//            }
-//
-//            Cache cache = new Cache(getCacheDir(), cacheSize);
-//            OkHttpClient okHttpClient = new OkHttpClient.Builder()
-//                    .cache(cache)
-//                    .addInterceptor(new Interceptor() {
-//                        @Override
-//                        public okhttp3.Response intercept(Interceptor.Chain chain) throws IOException {
-//                            Request request = chain.request();
-//                            if (!mConnectionDetector.isConnectingToInternet()){
-//                                int maxStale = 60 * 60 * 24 * 28; // tolerate 4-weeks stale \
-//                                request = request
-//                                        .newBuilder()
-//                                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
-//                                        .build();
-//                            }
-//
-//                            return chain.proceed(request);
-//                        }
-//                    })
-//                    .build();
-//
-//
-//            //create retrofit object
-//            Retrofit retrofit = new Retrofit.Builder()
-//                    .baseUrl(Api.BASE_URL)
-//                    .client(okHttpClient)
-//                    .addConverterFactory(GsonConverterFactory.create())
-//                    .build();
-//
-//            //build our API
-//            Api api = retrofit.create(Api.class);
-//
-//            //now making the call object
-//            //Here we are using the api method that we created inside the api interface
-//            Call<List<HeroModel>> call = api.getHeroes();
-//
-//            //then finallly we are making the call using enqueue()
-//            //it takes callback interface as an argument
-//            //and callback is having two methods onRespnose() and onFailure
-//            //if the request is successfull we will get the correct response and onResponse will be executed
-//            //if there is some error we will get inside the onFailure() method
-//
-//
-//            progressDialog = new ProgressDialog(MainActivity.this);
-//            progressDialog.setMax(100);
-//            progressDialog.setMessage("Its loading....");
-//            progressDialog.setTitle("ProgressDialog bar example");
-//            progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-//            // show it
-//            progressDialog.show();
-//
-//
-//            call.enqueue(new Callback<List<HeroModel>>() {
-//                @Override
-//                public void onResponse(Call<List<HeroModel>> call, Response<List<HeroModel>> response) {
-//                    //get the list of heroes
-//
-//                    List<HeroModel> heroesList = response.body();
-//
-//                    String[] heroes = new String[heroesList.size()];
-//
-//                    for (int i = 0; i < heroesList.size(); i++) {
-//                        heroes[i] = heroesList.get(i).getName();
-//                    }
-//
-//                    heroAdapter = new HeroAdapter(heroesList, getApplicationContext());
-//                    recyclerView.setAdapter(heroAdapter);
-//
-//                    progressDialog.dismiss();
-//
-//                }
-//
-//                @Override
-//                public void onFailure(Call<List<HeroModel>> call, Throwable t) {
-//                    Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
-//                    progressDialog.dismiss();
-//
-//                }
-//            });
-//        }catch (Exception e){
-//            Log.d("Error", e.getMessage());
-//            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
-//        }
-//    }
-
 
 }
