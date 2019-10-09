5ff76a7aa59cb51188fd77d9a75607c7588bbc6e
Afreen Siraj <afreen.siraj@capco.com>
2019-10-09 20:10:06 +0100

    Adding network class

---
+++ app/src/main/java/com/example/tw_test/network/ConnectionDetector.java
@@ -0,0 +1,22 @@
+package com.example.tw_test.network;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+public class ConnectionDetector {
+    private Context mContext;
+
+    public ConnectionDetector(Context context){
+        this.mContext = context;
+    }
+
+    public boolean isConnectingToInternet(){
+
+        ConnectivityManager cm = (ConnectivityManager)mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected() == true)
+        {
+            return true;
+        }
+        return false;
+    }
+}
